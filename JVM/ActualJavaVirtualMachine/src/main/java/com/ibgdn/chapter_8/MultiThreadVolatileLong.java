c7d67ebc8085cedb1da03aec6b529bfe023b64bf
ibgdn <ibgdn4@gmail.com>
2021-02-23 21:53:35 +0800

    Java 内存模型 原子性。

---
+++ JVM/ActualJavaVirtualMachine/src/main/java/com/ibgdn/chapter_8/MultiThreadVolatileLong.java
@@ -0,0 +1,64 @@
+package com.ibgdn.chapter_8;
+
+public class MultiThreadVolatileLong {
+    public static long t = 0;
+
+    public static class ChangeT implements Runnable {
+        private long to;
+
+        public ChangeT(long to) {
+            this.to = to;
+        }
+
+        /**
+         * When an object implementing interface <code>Runnable</code> is used
+         * to create a thread, starting the thread causes the object's
+         * <code>run</code> method to be called in that separately executing
+         * thread.
+         * <p>
+         * The general contract of the method <code>run</code> is that it may
+         * take any action whatsoever.
+         *
+         * @see Thread#run()
+         */
+        @Override
+        public void run() {
+            while (true) {
+                MultiThreadVolatileLong.t = to;
+                Thread.yield();
+            }
+        }
+    }
+
+    public static class ReadT implements Runnable {
+        /**
+         * When an object implementing interface <code>Runnable</code> is used
+         * to create a thread, starting the thread causes the object's
+         * <code>run</code> method to be called in that separately executing
+         * thread.
+         * <p>
+         * The general contract of the method <code>run</code> is that it may
+         * take any action whatsoever.
+         *
+         * @see Thread#run()
+         */
+        @Override
+        public void run() {
+            while (true) {
+                long tmp = MultiThreadVolatileLong.t;
+                if (tmp != 111L && tmp != -999L && tmp != 333L && tmp != -444L) {
+                    System.out.println(tmp);
+                }
+                Thread.yield();
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        new Thread(new ChangeT(111L)).start();
+        new Thread(new ChangeT(-999L)).start();
+        new Thread(new ChangeT(333L)).start();
+        new Thread(new ChangeT(-444L)).start();
+        new Thread(new ReadT()).start();
+    }
+}
